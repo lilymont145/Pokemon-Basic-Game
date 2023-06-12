6e6298e08012df0a8fcf9fb1db1e492db3993589
lilymont145 <128333196+lilymont145@users.noreply.github.com>
2023-06-12 20:46:37 +0700

    Update struct.h

--- struct.h
+++ struct.h
@@ -6,48 +6,60 @@
 #include<iomanip>
 #include<cstdlib>
 #include<ctime>
-#define BOARDWIDTH 8
-#define BOARDHEIGTH 5
+#include<conio.h>
+#include<fstream>
+
+#define PADDING 500 // bytes
+#define NAMESIZE 100
+#define PASSSIZE 100
+#define BOARDSIZE 999
+#define URLSIZE 100
 #define KEY_UP 72
 #define KEY_DOWN 80
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
 #define ESC_KEY 27
-#define ENTER_KEY 13
-
+#define ENTER_KEY 97
+#define SHUFFLE_KEY 115
+#define HELP_KEY 98
+#define HACK_KEY 107
 using namespace std;
-struct position ;
-
-struct player;
-
-struct CELL1;
-
-struct CELL2;
 
 struct position {
 	int x, y;
 };
 
 struct player {
-	string name = "";
-	int point, life;
+	char name[NAMESIZE];
+	int point;
 };
 
-struct CELL_1 {
-	int i, j;
-	char c = ' ';
-	bool isNotEmpty = 1, isSelected = 0;
-
-	void drawBox(int);
-	void deleteBox();
+struct saveState
+{
+	int* pos;
+	char* saveLetter;
 };
 
-struct CELL_2 {
+struct CELL1
+{
 	int i, j;
-	char c = ' ';
-	bool isSelected = 0;
-	CELL_2* next;
-
+	char letter = ' ';
+	bool isNotEmpty = true;
+	bool isSelected = false;
 	void drawBox(int);
 	void deleteBox();
+	void drawBG();
+	void highlightMove(int);
+};
+
+struct CELL2
+{
+	int i, j;
+	char letter = ' ';
+	bool isSelected = false;
+	CELL2* pNext;
+	void drawBox(int);
+	void deleteBox();
+	void drawBG();
+	void highlightMove(int);
 };
