bfd8cc49072e2b4db6c807ebc821c587b4c23bbd
lilymont145 <128333196+lilymont145@users.noreply.github.com>
2023-06-12 20:39:59 +0700

    Update console.h

--- console.h
+++ console.h
@@ -4,3 +4,4 @@ void goToXY(int rowPos, int colPos);
 void setCursorToConsole(bool visible, DWORD size);
 void resizeWindow(int x, int y);
 void initWindow(int length, int width);
+bool allcheck(CELL1** board, int x1, int y1, int x2, int y2, int& BOARDHEIGTH, int& BOARDWIDTH);
